e4f989489916b0533ab67d36a67a03b39f0edd60
hopeless-ponderer <hopelessponderer1123@gmail.com>
2021-07-07 22:10:53 -0700

    created Carthaginian "Mass Defection" tech + TechTransferOwnershipEntities Trigger function

--- simulation/components/Trigger~tech_triggers.js
+++ simulation/components/Trigger~tech_triggers.js
@@ -21,3 +21,31 @@ Trigger.prototype.TechSpawnUnits = function({ researcher, player, unitTemplate, 
 			"count": numRemaining
 		});
 };
+
+// transfer <ratio> percentage of entities of class <entityClass> to player <receivingPlayerID> from each player with diplomatic relationship <givingPlayersDiplo>, as long as the other player has <minEntities> of <entityClass>
+// @param receivingPlayerID			Number			ID of player who will received transferred entities.
+// @param givingPlayersDiplo			[String]		The diplomatic relationship the receiving player must have with each other player in order to receive their entities.
+// 													Possible values include: Ally, ExclusiveAlly, MutualAlly, ExclusiveMutualAlly, Enemy, Neutral.
+// @param entityClasses				[String]		Classes of entities that can be transferred.
+// @param ratio						Number			The percentage of each other player's entities to transfer to player <receivingPlayerID>. The exact entities will be chosen randomly.
+// 													Must be a decimal value between 0 and 1 (i.e. 0.3).
+// @param minEntities				Number			The minimum number of eligible entities each other player must have in order to lose them to the receiving player.
+Trigger.prototype.TechTransferOwnershipEntities = function({ receivingPlayerID, givingPlayersDiplo, entityClasses, ratio, minEntities })
+{
+	const cmpPlayerManager = Engine.QueryInterface(SYSTEM_ENTITY, IID_PlayerManager);
+	if (!cmpPlayerManager)
+		return;
+	const cmpReceivingPlayer = Engine.QueryInterface(cmpPlayerManager.GetPlayerByID(receivingPlayerID), IID_Player);
+	const givingPlayers = cmpPlayerManager.GetAllPlayers().filter(id => id !== receivingPlayerID).filter(id => givingPlayersDiplo.some(diplo => cmpReceivingPlayer["Is" + diplo](id)));
+	if (!givingPlayers || !givingPlayers.length)
+		return;
+	const cmpRangeManager = Engine.QueryInterface(SYSTEM_ENTITY, IID_RangeManager);
+	for (let id of givingPlayers)
+	{
+		const transferrableEntities = cmpRangeManager.GetEntitiesByPlayer(id).filter(ent => MatchesClassList(Engine.QueryInterface(ent, IID_Identity)?.GetClassesList(), entityClasses));
+		if (transferrableEntities.length < minEntities)
+			continue;
+		const entitiesToTransfer = shuffleArray(transferrableEntities).slice(0, transferrableEntities.length * ratio);
+		entitiesToTransfer.map(ent => Engine.QueryInterface(ent, IID_Ownership)).filter(cmp => cmp).forEach(cmpOwnership => cmpOwnership.SetOwner(receivingPlayerID));
+	}
+};
